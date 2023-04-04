48a3214381ac61b2945ae5997d737faa364793c4
west1 <eonias189@yandex.ru>
2023-04-04 17:24:38 +0400

    api v2

--- data/users_resource.py
+++ data/users_resource.py
@@ -1,4 +1,5 @@
-from flask_restful import reqparse, abort, Resource
+import datetime as dt
+from flask_restful import abort, Resource
 from flask import jsonify
 from . import db_session
 from .users import User
@@ -23,6 +24,33 @@ class UsersResource(Resource):
         user = db_sess.query(User).get(user_id)
         return jsonify({'user': user.to_dict(only=USER_TO_DICT_ONLY)})
 
+    def put(self, user_id):
+        abort_if_not_founded(user_id)
+        db_sess = db_session.create_session()
+        parser = UserParser()
+        args = parser.parse_args()
+        user = db_sess.query(User).get(user_id)
+        id_was = db_sess.query(User).get(args['id'])
+        if id_was and id_was.id != user_id:
+            return jsonify({'error': 'Id is already taken'})
+        user_was = db_sess.query(User).filter(
+            User.email == args['email']).first()
+        if user_was and user_was.email != user.email:
+            return jsonify({'error': 'email is already taken'})
+        user.id = args['id']
+        user.surname = args['surname']
+        user.name = args['name']
+        user.age = args['age']
+        user.position = args['position']
+        user.speciality = args['speciality']
+        user.address = args['address']
+        user.email = args['email']
+        user.modified_date = dt.datetime.strptime(args['modified_date'],
+                                                  '%Y-%m-%d %H:%M:%S.%f')
+        user.set_password(args['hashed_password'])
+        db_sess.commit()
+        return jsonify({'success': 'OK'})
+
     def delete(self, user_id):
         abort_if_not_founded(user_id)
         db_sess = db_session.create_session()
@@ -38,3 +66,22 @@ class UserListResource(Resource):
         users = db_sess.query(User).all()
         return jsonify({'users': [
             [user.to_dict(only=USER_TO_DICT_ONLY) for user in users]]})
+
+    def post(self):
+        parser = UserParser()
+        args = parser.parse_args()
+        db_sess = db_session.create_session()
+        if db_sess.query(User).get(args['id']):
+            return jsonify({'error': 'Id is already taken'})
+        if db_sess.query(User).filter(User.email == args['email']).first():
+            return jsonify({'error': 'email is already taken'})
+        user = User(id=args['id'], surname=args['surname'], name=args['name'],
+                    age=args['age'], position=args['position'],
+                    speciality=args['speciality'], address=args['address'],
+                    email=args['email'],
+                    modified_date=dt.datetime.strptime(args['modified_date'],
+                                                       '%Y-%m-%d %H:%M:%S.%f'))
+        user.set_password(args['hashed_password'])
+        db_sess.add(user)
+        db_sess.commit()
+        return jsonify({'success': 'OK'})
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from pprint import pprint
 import datetime as dt
+from tools import hash_password
 import requests
 
+js = {'id': 6, 'surname': 'surname', 'name': 'name_', 'age': 30,
+      'position': 'position', 'speciality': 'speciality',
+      'address': 'address', 'email': 'eonias',
+      'hashed_password': hash_password('qwerty'),
+      'modified_date': str(dt.datetime.now())}
+
 url = 'http://127.0.0.1:5000/api/v2/users/6'
-response = requests.delete(url)
-print(response.json())
+response = requests.put(url, json=js)
+pprint(response.json())
